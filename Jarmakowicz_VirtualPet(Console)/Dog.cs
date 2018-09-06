e00cf47cee2dddc117b81991c6c3310db0ee0e7d
Claudia <c.jar136@gmail.com>
2018-09-06 14:55:33 -0500

    fixed

---
+++ Jarmakowicz_VirtualPet(Console)/Dog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jarmakowicz_VirtualPet_Console_
+{
+    class Dog: Pet
+    {
+
+        public override string Talk()
+        {
+            return "Bark!";
+        }
+    }
+}
